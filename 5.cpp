7d44bb8c61d2bccd6b69756fb75de27c818a39a4
Suraj Kumar Patel <42140899+suraj8730@users.noreply.github.com>
2018-09-06 20:57:13 +0530

    Add files via upload

---
+++ 5.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+int main()
+	{
+	for(int i=0;i<5;i++)
+		{ for(int a=5;a>0;a--)
+			{cout<<" ";}
+		for(int b=0;b<1;b++)
+			{cout<<"*";}
+		if (i=0 or i=4)for (int j=0;j<<( (2*i)+1);j++)
+			{cout<<" ";}
+		for(int b=0;b<1;b++)
+			{cout<<"*";}
+		//for space
+		//for star
+		//for space
+		//for star
+		cout<<endl;
+}
+}
